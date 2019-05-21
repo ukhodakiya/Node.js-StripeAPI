59821748a5a970637de8051ddf8ec1e36fae6f4c
Umang <ukhodakiya@gmail.com>
2019-05-21 12:56:12 -0400

    update

---
+++ app.js
@@ -0,0 +1,49 @@
+// publish key : pk_test_yDa5Ew4gZYXaGcCKumAy0NaB00zXM38NLQ
+// secret key : sk_test_zk7iNz1Ey8jumh5MPIYIj1GO00BbxqciJg
+const express = require("express");
+
+const stripe = require("stripe")("sk_test_zk7iNz1Ey8jumh5MPIYIj1GO00BbxqciJg");
+
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const app = express();
+
+//handlebar middleware
+app.engine('handlebars',exphbs({defaultLayout: 'main'}));
+app.set('view engine','handlebars');
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//set static folder
+app.use(express.static(`${__dirname}/public`));
+
+//index route
+app.get("/",(req,res) => {
+   res.render('index');
+});
+
+
+//charge route
+app.post("/charge",(req,res) => {
+   const amount = 3000;
+   // console.log(req.body);
+   // res.send('test');
+   stripe.customers.create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken
+   })
+       .then(customer => stripe.charges.create({
+          amount,
+          description:"Life's Amazing Secrets",
+          currency:"usd",
+          customer:customer.id
+       }))
+       .then(charge => res.render("success"));
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+   console.log(`Server started on port ${port}`);
+});
